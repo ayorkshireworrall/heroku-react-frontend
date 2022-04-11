d28f6ea52de08f65ff68d8d8687072ef30409165
aworrall <alex.worrall@keytree.co.uk>
2022-04-11 19:37:13 +0100

    front end logging to investigate env variable

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const dispatch = useDispatch();
   const onInitMessages = useCallback(() => dispatch(actions.getMessages()), [dispatch]);
 
+  console.log('configuration: ', Config)
+
   useEffect(() => {
     onInitMessages();
   }, [onInitMessages])
